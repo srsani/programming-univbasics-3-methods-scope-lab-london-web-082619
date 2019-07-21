47194eb275cf6f15d777db6f82d10cd9858b39ed
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 08:00:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario(phrase = "It's-a me, Mario!")
-  return status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
