0c5c03d980489e39976785a90d97c60f55b1c966
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 08:04:46 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,16 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def mario(phrase = "It's-a me, Mario!")
   puts phrase
 end
 
-def toadstool
-  puts status
+def toadstool()
+  puts $status
 end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end 
+
+def any_phrase (any_phrase)
+  puts any_phrase
+end 
