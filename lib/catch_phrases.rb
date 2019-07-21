c9d18700b00a0f5b54aafc3a7510d3ea017cc3c1
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 08:01:47 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,6 @@ def mario(phrase = "It's-a me, Mario!")
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
